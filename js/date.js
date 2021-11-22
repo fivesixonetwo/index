f75e53530001d46e54e0a45ee2d84f6e824c5bcb
Huỳnh Việt Tiến <15520877@gm.uit.edu.vn>
2021-11-23 00:47:04 +0700

    fix

--- js/date.js
+++ js/date.js
@@ -1,1 +1,22 @@
-const getMinDate=function(){let e=new Date;const t=e.toDateString().split(" ");return("01"===t[2]?e.getUTCMonth()+2:e.getUTCMonth()+1)+"/"+t[2]+"/"+e.getUTCFullYear()};$(".searchbar__checkin--in").daterangepicker({minDate:getMinDate()},function(e,t){console.log("New date range selected: "+e.format("YYYY-MM-DD")+" to "+t.format("YYYY-MM-DD"))}),$(".searchbar__checkin--out").daterangepicker({opens:"left",minDate:getMinDate()},function(e,t){console.log("New date range selected: "+e.format("YYYY-MM-DD")+" to "+t.format("YYYY-MM-DD"))});
+const getMinDate = function () {
+    let dateObj = new Date();
+    const arr = dateObj.toDateString().split(" ")
+    let month = arr[2] === "01" ? dateObj.getUTCMonth() + 2 : dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = arr[2];
+    let year = dateObj.getUTCFullYear();
+    let newdate = month + "/" + day + "/" + year;
+    return newdate;
+}
+
+$(".searchbar__checkin--in").daterangepicker({
+    "minDate": getMinDate(),
+}, function (start, end) {
+    
+    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+});
+$(".searchbar__checkin--out").daterangepicker({
+    "opens": "left",
+    "minDate": getMinDate()
+}, function (start, end) {
+    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+});
